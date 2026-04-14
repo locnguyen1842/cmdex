bf9dbc25ad6ddb2f0096afe252462a6e48110115
locnguyen1842 <locnguyen1842@gmail.com>
2026-04-14 14:29:43 +0700

    feat(events): create events.ts with global event name constants

---
+++ frontend/src/wails/events.ts
@@ -0,0 +1,4 @@
+export const EVENT_CMD_OUTPUT = 'cmd-output';
+export const EVENT_OPEN_SETTINGS = 'open-settings';
+export const EVENT_SETTINGS_CHANGED = 'settings-changed';
+export const EVENT_SETTINGS_WINDOW_HIDING = 'settings-window-hiding';
