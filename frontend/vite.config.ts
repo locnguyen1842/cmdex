55137b68e659dd81b8464b81283e0ac6e6356d40
locnguyen1842 <locnguyen1842@gmail.com>
2026-04-15 14:49:50 +0700

    chore: update task env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,21 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import wails from "@wailsio/runtime/plugins/vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss(), wails("./bindings")],
-  server: {
-    port: 9245,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss(), wails("./bindings")],
+    server: {
+      port: parseInt(env.VITE_PORT) || 9245,
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }
 })
