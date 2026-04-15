433037aa3f89be54ee59ccbffa21883f11a42daa
locnguyen1842 <locnguyen1842@gmail.com>
2026-04-15 12:29:59 +0700

    feat: update build commands

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,13 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import wails from "@wailsio/runtime/plugins/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), wails("./bindings")],
+  server: {
+    port: 9245,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
